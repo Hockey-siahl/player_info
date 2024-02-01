4b82acc3b915081d990c6619203378a7de563584
Susan Kulawik <60016019+sundse@users.noreply.github.com>
2024-02-01 10:28:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,14 @@ function findPlayer() {
             // Do something with the filtered players
             player.forEach(playerx => {
                 console.log(playerx.player_name);
-            });            
-        document.getElementById('playerName').textContent = player[0].player_name;
+            });
+        if (player.length == 0) {document.getElementById('playerName1').textContent = 'No players found';}
+        if (player.length > 0) {document.getElementById('playerName1').textContent = player[0].player_name;}
+        if (player.length > 1) {document.getElementById('playerName2').textContent = player[1].player_name;}
+        if (player.length > 2) {document.getElementById('playerName3').textContent = player[2].player_name;}
+        if (player.length > 3) {document.getElementById('playerName4').textContent = player[3].player_name;}
+        if (player.length > 4) {document.getElementById('playerName4').textContent = player[3].player_name;}
+      
         })
         .catch(error => {
             console.error('Error reading JSON:', error);
