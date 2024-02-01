8dccfbb06f691c7b616727bb8d0dada3adc23845
Susan Kulawik <60016019+sundse@users.noreply.github.com>
2024-02-01 14:12:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,24 +12,20 @@ function findPlayer() {
   let playerNumber = Number(document.getElementById('playerNumber').value);
   console.log(playerNumber);
   
-  fetch("players.json")
+  fetch("player_data_website.json")
     .then(response => response.json())
     .then(data => {
             // Filter players with jersey_number playerNumber
-            let player = data.filter(player => player.player_number == playerNumber);
+            let player = data.filter(player => player.jersey_number == playerNumber);
 
-            // Do something with the filtered players
-            player.forEach(playerx => {
-                console.log(playerx.player_name);
-            });
-        console.log(`Player Name: ${player.player_name}, Team Name: ${player.team_name}`);
+        console.log(`Player Name: ${player.info}`);
 
         if (player.length == 0) {document.getElementById('playerName1').textContent = 'No players found';}
-        if (player.length > 0) {document.getElementById("playerName1").textContent = `${player[0].player_name} ${player[0].team_name}`;}
-        if (player.length > 1) {document.getElementById("playerName2").textContent = `${player[1].player_name} ${player[1].team_name}`;}
-        if (player.length > 2) {document.getElementById("playerName3").textContent = `${player[2].player_name} ${player[2].team_name}`;}
-        if (player.length > 3) {document.getElementById("playerName4").textContent = `${player[3].player_name} ${player[3].team_name}`;}
-        if (player.length > 4) {document.getElementById("playerName4").textContent = `${player[4].player_name} ${player[4].team_name}`;}
+        if (player.length > 0) {document.getElementById("playerName1").textContent = player[0].info;}
+        if (player.length > 1) {document.getElementById("playerName2").textContent = player[1].info;}
+        if (player.length > 2) {document.getElementById("playerName3").textContent = player[2].info;}
+        if (player.length > 3) {document.getElementById("playerName4").textContent = player[3].info;}
+        if (player.length > 4) {document.getElementById("playerName4").textContent = player[4].info;}
       
         })
         .catch(error => {
