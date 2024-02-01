f5e7c940ee93f2e06b43ad3b42ce44e02dc23b85
Susan Kulawik <60016019+sundse@users.noreply.github.com>
2024-01-31 21:40:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function findPlayer() {
   let playerNumber = document.getElementById('playerNumber').value;
-  document.getElementById('playerName').textContent = '10';
+
+  fetch('players.json')
+    .then(response => response.json())
+    .then(data => {
+            // Filter players with jersey_number 15
+            let player = data.filter(player => player.jersey_number == playerNumber);
+
+            // Do something with the filtered players
+            console.log(player);
+            document.getElementById('playerName').textContent = player => player_name;
+        })
+        .catch(error => {
+            console.error('Error reading JSON:', error);
+        });
+}
+  
 }
