854ed9a5f2d89bc24fd6f5a0be35a28543e9dbce
Susan Kulawik <60016019+sundse@users.noreply.github.com>
2024-02-01 11:03:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ function findPlayer() {
         console.log(`Player Name: ${player.player_name}, Team Name: ${player.team_name}`);
 
         if (player.length == 0) {document.getElementById('playerName1').textContent = 'No players found';}
-        if (player.length > 0) {document.getElementById(`playerName1').textContent = '${player[0].player_name} ${player[0].team_name}`;}
-        if (player.length > 1) {document.getElementById(`playerName2').textContent = '${player[1].player_name} ${player[1].team_name}`;}
-        if (player.length > 2) {document.getElementById(`playerName3').textContent = '${player[2].player_name} ${player[2].team_name}`;}
-        if (player.length > 3) {document.getElementById(`playerName4').textContent = '${player[3].player_name} ${player[3].team_name}`;}
-        if (player.length > 4) {document.getElementById(`playerName4').textContent = '${player[4].player_name} ${player[4].team_name}`;}
+        if (player.length > 0) {document.getElementById("playerName1").textContent = `${player[0].player_name} ${player[0].team_name}`;}
+        if (player.length > 1) {document.getElementById("playerName2").textContent = `${player[1].player_name} ${player[1].team_name}`;}
+        if (player.length > 2) {document.getElementById("playerName3").textContent = `${player[2].player_name} ${player[2].team_name}`;}
+        if (player.length > 3) {document.getElementById("playerName4").textContent = `${player[3].player_name} ${player[3].team_name}`;}
+        if (player.length > 4) {document.getElementById("playerName4").textContent = `${player[4].player_name} ${player[4].team_name}`;}
       
         })
         .catch(error => {
