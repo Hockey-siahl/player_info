136619d0322c7f18cb082076285c6595b7524785
Susan Kulawik <60016019+sundse@users.noreply.github.com>
2024-02-04 09:15:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function initializeDropdown8B(data) {
     let teamDropdown = document.getElementById('team8BDropdown');
-    #let teams = new Set(data.map(player => player.team_name));
+    //let teams = new Set(data.map(player => player.team_name));
     let teams = new Set(data.team)
     teams.forEach(team => {
         let option = document.createElement('option');
